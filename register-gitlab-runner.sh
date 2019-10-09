a8d388af88e228108ec381f4311eeb9b293d7930
Alexander Rettner <arettner@web.de>
2019-10-09 17:55:52 +0200

    docker compose to run gitlab plus runner

---
+++ register-gitlab-runner.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+docker run --rm \
+    --network gitlab-test_default \
+    --volume gitlab-test_gitlab-runner-config:/etc/gitlab-runner gitlab/gitlab-runner:alpine-v12.3.0 register \
+        --non-interactive \
+        --executor "docker" \
+        --docker-image alpine:3.10.2 \
+        --docker-network-mode gitlab-test_default \
+        --url "http://gitlab/" \
+        --registration-token "U9ioJ5gcMMsy1Z6snDg7" \
+        --description "docker" \
+        --tag-list "docker,my-tag,yet-another-tag" \
+        --run-untagged="true" \
+        --locked="false" \
+        --access-level="not_protected"
